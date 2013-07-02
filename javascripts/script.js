78a61dd65e60a2f388678b534831819ab7d5d67d
Alyssa <alyhat@gmail.com>
2013-07-02 00:30:23 -0400

    Update script.js

--- javascripts/script.js
+++ javascripts/script.js
@@ -1,52 +1,34 @@
-(function($) {
-$(document).ready(function(){
-
-  // putting lines by the pre blocks
-  $("pre").each(function(){
-    var pre = $(this).text().split("\n");
-    var lines = new Array(pre.length+1);
-    for(var i = 0; i < pre.length; i++) {
-      var wrap = Math.floor(pre[i].split("").length / 70)
-      if (pre[i]==""&&i==pre.length-1) {
-        lines.splice(i, 1);
-      } else {
-        lines[i] = i+1;
-        for(var j = 0; j < wrap; j++) {
-          lines[i] += "\n";
-        }
-      }
+_init : function( options ) {
+ 
+    // the categories (ul)
+    this.$categories = this.$el.children( 'ul' );
+    // the navigation
+    this.$navcategories = this.$el.find( 'nav > a' );
+    var animEndEventNames = {
+        'WebkitAnimation' : 'webkitAnimationEnd',
+        'OAnimation' : 'oAnimationEnd',
+        'msAnimation' : 'MSAnimationEnd',
+        'animation' : 'animationend'
+    };
+    // animation end event name
+    this.animEndEventName = animEndEventNames[ Modernizr.prefixed( 'animation' ) ];
+    // animations and transforms support
+    this.support = Modernizr.csstransforms && Modernizr.cssanimations;
+    // if currently animating
+    this.isAnimating = false;
+    // current category
+    this.current = 0;
+    var $currcat = this.$categories.eq( 0 );
+    if( !this.support ) {
+        this.$categories.hide();
+        $currcat.show();
     }
-    $(this).before("<pre class='lines'>" + lines.join("\n") + "</pre>");
-  });
-
-  var headings = [];
-
-  var collectHeaders = function(){
-    headings.push({"top":$(this).offset().top - 15,"text":$(this).text()});
-  }
-
-  if($(".markdown-body h1").length > 1) $(".markdown-body h1").each(collectHeaders)
-  else if($(".markdown-body h2").length > 1) $(".markdown-body h2").each(collectHeaders)
-  else if($(".markdown-body h3").length > 1) $(".markdown-body h3").each(collectHeaders)
-
-  $(window).scroll(function(){
-    if(headings.length==0) return true;
-    var scrolltop = $(window).scrollTop() || 0;
-    if(headings[0] && scrolltop < headings[0].top) {
-      $(".current-section").css({"opacity":0,"visibility":"hidden"});
-      return false;
+    else {
+        $currcat.addClass( 'mi-current' );
     }
-    $(".current-section").css({"opacity":1,"visibility":"visible"});
-    for(var i in headings) {
-      if(scrolltop >= headings[i].top) {
-        $(".current-section .name").text(headings[i].text);
-      }
-    }
-  });
-
-  $(".current-section a").click(function(){
-    $(window).scrollTop(0);
-    return false;
-  })
-});
-})(jQuery)
+    // current nav category
+    this.$navcategories.eq( 0 ).addClass( 'mi-selected' );
+    // initialize the events
+    this._initEvents();
+ 
+}
